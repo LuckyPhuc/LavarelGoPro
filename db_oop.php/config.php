ad1605f7545e7c82e25e2236673d42f498dc12ba
LuckyPhuc <bmtck0000@gmail.com>
2023-11-17 10:51:31 +0700

    database connect

--- db_oop.php/config.php
+++ db_oop.php/config.php
@@ -1,7 +1,7 @@
 <?
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'nguyenphuc0062');
 define('DB_NAME', 'lavarelpro');
 /* define('DB_HOST','localhost'); - Định nghĩa hằng số DB_HOST với giá trị 'localhost'. Trong hầu hết các trường hợp, localhost chỉ đến máy chủ cơ sở dữ liệu chạy cùng máy với máy chủ web.
 define('DB_USER','root'); - Định nghĩa hằng số DB_USER với giá trị 'root'. Đây là tên người dùng được sử dụng để kết nối với cơ sở dữ liệu. root thường là tên người dùng mặc định có quyền cao nhất trên máy chủ cơ sở dữ liệu.
